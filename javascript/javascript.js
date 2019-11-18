17664ddc1e6b3b573cb56c5f002eb70bbbbc7ac5
farid.abdalla <farid.abdalla@he-arc.ch>
2019-11-18 17:25:43 +0100

    Test with choropleth

---
+++ javascript/javascript.js
@@ -0,0 +1,128 @@
+//#region variables
+var mymap = L.map('mapid').setView([46.81, 8.22], 5);
+var countries;
+var geojson;
+var info = L.control();
+//#endregion
+
+//#region Logic functions
+
+function setAverageForCountries(countries)
+{
+    countries.forEach(element => {
+        element.properties.average = getAverageScore(element.properties.ADMIN)
+    });
+}
+
+function getAverageScore(countryName)
+{
+    return Math.random() * 5;
+}
+
+//#endregion
+
+//#region Map Interaction
+function getColor(d) {
+    return d > 5 ? '#800026' :
+           d > 4  ? '#BD0026' :
+           d > 3  ? '#E31A1C' :
+           d > 2  ? '#FC4E2A' :
+           '#FD8D3C';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.average),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        //click: zoomToFeature
+    });
+}
+//#endregion
+
+//#region Map Creation
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: 'pk.eyJ1Ijoia3Vyb2thYmUiLCJhIjoiY2szNGsxNDl2MDJ3cTNvbXZucGZibG5oOSJ9.AC23cXaWGHa-venrXcvK5A'
+}).addTo(mymap);
+
+
+$.getJSON("./data/countries.geojson", function(json) {
+    countries = json.features;
+    setAverageForCountries(countries);
+    geojson = L.geoJson(countries, {style: style, onEachFeature: onEachFeature}).addTo(mymap);
+});
+//#endregion
+
+//#region Info display
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Beer average</h4>' +  (props ?
+        '<b>' + props.ADMIN + '</b><br />' + props.average
+        : 'Hover over a country');
+};
+
+info.addTo(mymap);
+//#endregion
+
+//#region Legend
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(mymap);
+//#endregion
